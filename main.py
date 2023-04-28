a689ecd30e1258bbc643b5cc2f1cdabea3ba9588
felice1004 <palette1004@gmail.com>
2023-04-28 10:18:34 +0800

    revise info

--- main.py
+++ main.py
@@ -98,7 +98,8 @@ with st.sidebar:
   st.write('3. 點擊你目前正在整理的 Section，例如"Assign AE"')
   st.write('4. 在頁面底部找到 Export to CSV 的按鈕（如下），點擊後會跳出下載小視窗，再點擊裡面的 "Click" 即可下載 CSV 檔')  
   st.image(Image.open('2.png'),width=150)
-  st.write('5. 回到這裡，上傳剛剛下載的CSV檔即可！')  
+  st.write('5. 回到這裡，上傳剛剛下載的CSV檔')  
+  st.write('6. 按下執行按鈕')  
   st.info('備註：建議使用 Google Sheet 開啟完成的檔案，這樣才不會有亂碼喔！(會儘快修復這個小bug 🥺)')  
   st.warning('注意：如果 note 欄位出現負數，代表 reviewes required to make decision 可能不為 2，需要再人工查詢，調整天數', icon="⚠️")
 
