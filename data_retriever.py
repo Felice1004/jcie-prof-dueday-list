7baf9ccdc1433e2977a063fd4b6748c6b8821d06
felice1004 <palette1004@gmail.com>
2023-05-11 15:18:07 +0800

    modulization

--- data_retriever.py
+++ data_retriever.py
@@ -1,4 +1,4 @@
 def get_paper_status(file_path):
-  with open('file_path', 'r') as f:
+  with open(file_path, 'r') as f:
     paper_status = [line.strip() for line in f.readlines()]
     return paper_status
